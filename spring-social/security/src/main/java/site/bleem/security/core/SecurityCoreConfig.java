456435d17f86fffea3933535a3f654ef80176576
ripley <ripleyman@163.com>
2023-07-20 02:06:31 +0800

    feat: 增加spring-social模块中的security

---
+++ spring-social/security/src/main/java/site/bleem/security/core/SecurityCoreConfig.java
@@ -0,0 +1,10 @@
+package site.bleem.security.core;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(SecurityProperties.class)
+public class SecurityCoreConfig {
+}
---
+++ spring-social/security/src/main/java/site/bleem/security/demo/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package site.bleem.security.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password = passwordEncoder.encode("111111");
+        auth.inMemoryAuthentication().withUser("user").password(password).roles("administrator");
+    }
+
+    @Bean
+    PasswordEncoder password() {
+        return new BCryptPasswordEncoder();
+    }
+}
