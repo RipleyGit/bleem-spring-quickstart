99c43d757358451541e4e05d20a13a494ffea880
ripley <ripleyman@163.com>
2023-09-14 15:42:12 +0800

    feat:spring-boot2/boot-vue/

---
+++ spring-boot2/boot-vue/src/main/java/site/bleem/boot/vue/BootVueApplication.java
@@ -0,0 +1,14 @@
+package site.bleem.boot.vue;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootVueApplication.class, args);
+    }
+
+}
---
+++ spring-boot2/boot-vue/src/main/java/site/bleem/boot/vue/controller/HelloController.java
@@ -0,0 +1,18 @@
+package site.bleem.boot.vue.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yubangshui
+ * @desc todo
+ * @date 2023/7/25
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> returnWorld() {
+        return ResponseEntity.ok("world!");
+    }
+}
