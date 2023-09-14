e0a74e5faa49195bacb6c313dd05f51e19e3ea66
ripley <ripleyman@163.com>
2023-09-14 15:40:51 +0800

    feat:spring-boot2/boot-web/

--- spring-boot2/boot-web/src/main/java/site/bleem/boot/hazelcast/HelloWorldApplication.java
+++ spring-boot2/boot-web/src/main/java/site/bleem/boot/hazelcast/HelloWorldApplication.java
@@ -3,9 +3,7 @@ package site.bleem.boot.hazelcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class HelloWorldApplication {
 
